8f41bd344822ab0bd8741aaa2e958d51f332a5e6
isaias.neto <isaias.neto@pcinformatica.com.br>
2017-12-20 15:38:49 -0200

    Aula 02 - Separando e filtrando mensagens

--- src/main/java/br/com/alura/camel/RotaPedidos.java
+++ src/main/java/br/com/alura/camel/RotaPedidos.java
@@ -8,11 +8,15 @@ public class RotaPedidos extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("file:pedidos?delay=5s&noop=true")
-			.log("${id}")
-			.marshal().xmljson()
-			.log("${body}")
-			.setHeader("CamelFileName", simple("${file:name.noext}.json"))
-		.to("file:saida");
+		from("file:pedidos?delay=5s&noop=true").
+			split().
+				xpath("/pedido/itens/item").
+			filter()
+				.xpath("/pedido/itens/item/formato[text()='EBOOK']").
+			log("${id}").
+			marshal().xmljson().
+			log("${body}").
+			setHeader("CamelFileName", simple("${file:name.noext}.json")).
+		to("file:saida");
 	}
 }
